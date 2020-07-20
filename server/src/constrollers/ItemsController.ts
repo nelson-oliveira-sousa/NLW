6b38e6ad06035ab02a7754a31e3d31157d6717e9
nelso <nelson.oliveira.sousa@gmail.com>
2020-07-20 20:07:42 -0300

    Ecoleta

---
+++ server/src/constrollers/ItemsController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import Knex from '../database/connection';
+
+class ItemsController {
+    async index (request: Request,response: Response){
+        const items = await Knex('items').select('*');
+    
+        const serializeItems = items.map(item => {
+           return {
+            id: item.id,
+            title: item.title,
+            image_url: `http://192.168.11.6:3333/uploads/${item.image}`,
+           };
+        });
+    
+        return response.json(serializeItems);
+    }
+}
+
+export default ItemsController;
